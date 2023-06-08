a6bec776d6712d9870dd70c424120236f7fba15c
unknown <ra.mdsharifulislam@gmail.com>
2023-06-08 14:10:28 +0600

    project starting

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@
 
 function App() {
   return (
-    <div className="App">
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Button</button>
-      <button className="btn btn-secondary">Button</button>
-      <button className="btn btn-accent">Button</button>
-      <button className="btn btn-ghost">Button</button>
-      <button className="btn btn-link">Button</button>
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-tl from-rose-500 to-blue-600">
+      
+     <h1 className="font-extrabold text-4xl border py-5 px-5 rounded-sm capitalize">OSICL webside is under working</h1>
     </div>
   );
 }
